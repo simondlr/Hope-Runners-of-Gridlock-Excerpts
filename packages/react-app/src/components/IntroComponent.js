c48e0374fa8132a3036cc14d6aaebe34ab040834
Simon de la Rouviere <simon@delarouviere.com>
2020-12-06 11:06:28 +0200

    polished site

--- packages/react-app/src/components/IntroComponent.js
+++ packages/react-app/src/components/IntroComponent.js
@@ -18,49 +18,142 @@ import excerpt_13 from "./Excerpt_13.png";
 import excerpt_14 from "./Excerpt_14.png";
 import excerpt_15 from "./Excerpt_15.png";
 
+import cover from "./final.jpg";
+
 function IntroComponent(props) {
      
     return (
         <div className="App" style={{textAlign:"justify"}}> 
-        <h2>Hope Runners of Gridlock Excerpts</h2>
-        Own an excerpt from the novel, Hope Runners of Gridlock.
+        <h1>Excerpts of Gridlock</h1>
+        <hr />
+        Own a digital collectible excerpt from the novel, <a href="https://blog.simondlr.com/posts/hope-runners-of-gridlock">Hope Runners of Gridlock</a>.
+        Hope Runners of Gridlock is a story about finding hope and belonging in a cyberpunk city with radical markets. 
+        It's available on Gumroad and Amazon (pay-what-you-want) and released under creative commons.<br />
         <br />
-        <img src={excerpt_1} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_2} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_3} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_4} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_5} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_6} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_7} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_8} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_9} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_10} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_11} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_12} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_13} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_14} style={{maxWidth: "100%", maxHeight: "100%"}} />
-        <img src={excerpt_15} style={{maxWidth: "100%", maxHeight: "100%"}} />
+        <img src={cover} style={{maxWidth: "100%", maxHeight: "100%"}} />
+        <br /><br />
+        <h2>Excerpts Of Gridlock</h2>
+        Excerpts of Gridlock is a digital art project that allows readers to collect 15 unique and scarce excerpts from the novel.
+        This is a proof-of-concept to showcase that in creative works, content can be readily accesible, but still allow the creator to earn from it by selling unique, exclusive digital merchandise related to the creative work.
+        In this case, with fiction, a reader can own specific excerpts secured by the Ethereum blockchain.
         <br />
-
+        <h2>Collect</h2>
+        There are 15 excerpts available from the novel. The initial auction is being conducted on OpenSea, a non-fungible token marketplace.
+        <br />
+        <br />
+        <h3>*SPOILERS*</h3>
+        <br />
+        <h2>Excerpt 1</h2>
+        <img src={excerpt_1} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/1">Collect Excerpt 1 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 2</h2>
+        <img src={excerpt_2} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/2">Collect Excerpt 2 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 3</h2>
+        <img src={excerpt_3} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/3">Collect Excerpt 3 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 4</h2>
+        <img src={excerpt_4} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/4">Collect Excerpt 4 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 5</h2>
+        <img src={excerpt_5} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/5">Collect Excerpt 5 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 6</h2>
+        <img src={excerpt_6} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/6">Collect Excerpt 6 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 7</h2>
+        <img src={excerpt_7} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/7">Collect Excerpt 7 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 8</h2>
+        <img src={excerpt_8} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/8">Collect Excerpt 8 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 9</h2>
+        <img src={excerpt_9} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/9">Collect Excerpt 9 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 10</h2>
+        <img src={excerpt_10} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/10">Collect Excerpt 10 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 11</h2>
+        <img src={excerpt_11} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/11">Collect Excerpt 11 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 12</h2>
+        <img src={excerpt_12} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/12">Collect Excerpt 12 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 13</h2>
+        <img src={excerpt_13} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/13">Collect Excerpt 13 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 14</h2>
+        <img src={excerpt_14} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/14">Collect Excerpt 14 on OpenSea.</Button>
+        <br />
+        <br />
+        <h2>Excerpt 15</h2>
+        <img src={excerpt_15} style={{border:"1px solid black", maxWidth: "100%", maxHeight: "100%"}} />
+        <Button size={"large"} type={"primary"} target="_blank" href="https://opensea.io/assets/0x8e49000dff43b131d6f2e0e8cc3d081476f068dc/15">Collect Excerpt 15 on OpenSea.</Button>
+        <br />
+        <br />
         <hr />
         {/* TECHNICALS SECTION */}
         <div className="section">
         <h2>Technicals</h2>
+        The excerpts are secured according the ERC721 standard. The code is available on <a href="https://github.com/simondlr/Hope-Runners-of-Gridlock-Excerpts">Github</a> if anyone wishes to implement a similar project.
+        </div>
+        <hr />
+        {/* FAQ SECTION */}
+        <div className="section">
+        <h2>FAQ</h2>
+        <h3>Why not always-on-sale excerpts?</h3>
+        A core part of the novel covers always-on-sale cars in a gridlock. 
+        For this project, however, I wanted to stick with the proof-of-concept of creating collectible excerpts. If successful, I will consider creating an always-on-sale work related to the novel.
+        <br /><br />
+        <h3>Will this be all the Hope Runners of Gridlock Collectibles?</h3>
+        As mentioned above, I am working on additional collectibles related to the novel, but none of them would be direct excerpts from the novel in this format.
+        <br /><br />
+        <h3>Where can I read the novel?</h3>
+        <a href="https://blog.simondlr.com/posts/hope-runners-of-gridlock">More Details Here.</a> <a href="https://gumroad.com/l/oeMtP">Pay-what-you-want (fiat or crypto) on Gumroad</a> and <a href="https://www.amazon.com/dp/B08GY96CFR">$4.99 on Amazon.</a>
         </div>
         <hr />
         {/* ARTIST SECTION */}
         <div className="section">
-        <h2>About The Artist:</h2>
+        <h2>About The Creator:</h2>
         I'm a creator at heart.
-        I have created games, writing, music, code, companies, and new economics. Solving the problems of the creator 
+        I have created games, writing, music, code, companies, art, and new economics. Solving the problems of the creator 
         has always been important to me. In the past I co-founded Ujo Music,
         working with Grammy-winning artists such as Imogen Heap and RAC to launch the first music royalty projects using smart contracts. I've
         helped kickstart wholly new markets and economies. I helped to create the Ethereum ERC20 token standard and token bonding curves, technologies 
         that's currently facilitating economies worth several billion dollars of value.
-        I enjoy creating new forms of art and experimenting with ways to empower the creative industry.
+        I enjoy creating new forms of art and experimenting with ways to empower creatives.
+        I have more digital art available here. The first <a href="http://thisartworkisalwaysonsale.com/">artwork that is always on sale.</a>
         <br />
         <br />
         Swing me a follow on Twitter! <a href="https://twitter.com/simondlr">@simondlr</a>.<br />
+        Check my blog and newsletter! <a href="https://blog.simondlr.com">https://blog.simondlr.com</a>.<br />
         <br />
         </div>
         </div>
